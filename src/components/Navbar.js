d59d7c5f623589da8b8ed87328be3be106ca4e92
Mitchell Thatcher <58484785+MAThatcher@users.noreply.github.com>
2020-03-15 13:08:01 -0700

    Initial Commit
    Initial Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Navbar(){
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <img src="moon.png" height="40" width="40" />
+            <a class="navbar-brand">Luna Ticketing Program</a>
+        </nav>
+    )
+}
+
+export default Navbar
