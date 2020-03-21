19ef4e2becaebb415e4e11b959119da7cf95f0f6
Mitchell Thatcher <58484785+MAThatcher@users.noreply.github.com>
2020-03-21 14:27:39 -0700

    .
    .

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,24 +1,31 @@
 import React from "react"
+import Navbar from './Navbar.js';
+import Footer from './Footer.js';
 
 class Login extends React.Component{
+
     render(){
         return(
-            <body className="bg-transparent text-center text-white" >
-                <form className="form-signin">
-                    <img className="mb-4" src="../../public/moon.png" alt="" width="72" height="72"></img>
-                    <h1 className="h3 mb-3 font-weight-normal" >Log In</h1>
-                    <input type="email" id="inputEmail"  placeholder="Email address" required autofocus></input><br/>
-                    <input type="password" id="inputPassword"  placeholder="Password" required>
-                    </input><br></br><br></br>
-                    <button className="btn btn-lg btn-primary " type="submit">Sign in</button>
-                </form><br></br>
-                <form action="./newAccount.html">
-                    <button type="submit" className="btn btn-lg btn-primary "> Create Account </button>
-                </form><br></br>
-                <form action="">
-                <button type="submit" className="btn btn-lg btn-primary "> Forgot Password </button>
-                </form>
-            </body>
+            <div>    
+                <Navbar />
+                <body className="bg-transparent text-center text-white" >
+                    <form className="form-signin">
+                        <img className="mb-4" src="../../public/moon.png" alt="" width="72" height="72"></img>
+                        <h1 className="h3 mb-3 font-weight-normal" >Log In</h1>
+                        <input type="email" id="inputEmail"  placeholder="Email address" required autoFocus></input><br/>
+                        <input type="password" id="inputPassword"  placeholder="Password" required>
+                        </input><br></br><br></br>
+                        <button className="btn  btn-primary " type="submit">Sign in</button>
+                    </form><br></br>
+                    <form action="./newAccount.html">
+                        <button type="submit" className="btn  btn-primary "> Create Account </button>
+                    </form><br></br>
+                    <form action="">
+                    <button type="submit" className="btn  btn-primary "> Forgot Password </button>
+                    </form>
+                </body>
+                <Footer />
+            </div>
                 
             
         )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,42 @@
 import React from "react"
 
 class Navbar extends React.Component{
+    constructor(){
+        super()
+        this.state= {
+            size:40,
+            imageLocation:"./moon.png"
+        }
+        this.handleClick = this.handleClick.bind(this)
+        this.handleHover = this.handleHover.bind(this)
+        this.handleExit = this.handleExit.bind(this)
+    }
+
+    handleClick() { 
+        //return to menue
+    }
+
+    handleExit() {
+        this.setState(prevState => {
+            return {
+                imageLocation:"./moon.png"
+            }
+        })
+    }
+
+    handleHover() {
+        this.setState(prevState => {
+            return {
+                imageLocation:"./moonReversed.png"
+            }
+        })
+    }
+
     render(){
         return (
-            <nav className="navbar navbar-dark bg-dark border border-dark   " >
-                <img src="moon.png" height="40" width="40" />
-                <a className="navbar-brand">Luna Ticketing Program</a>
+            <nav className="navbar navbar-dark bg-dark border border-dark" >
+                <img onMouseEnter={this.handleHover} onMouseLeave= {this.handleExit} onClick={this.handleClick} src={this.state.imageLocation} height={40} width={40} />
+                <label className="navbar-brand">Luna Ticketing Program</label>
             </nav>
         )
     }
