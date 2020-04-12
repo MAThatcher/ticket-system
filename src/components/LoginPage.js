5d4a4dc2d68d183195ecc15c08c00102ef4493e6
Mitchell Thatcher <58484785+MAThatcher@users.noreply.github.com>
2020-04-12 15:27:59 -0700

    Testing heroku deployment
    Testing heroku deployment

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -68,6 +68,7 @@ class Login extends React.Component{
                             required 
                             autoFocus>
                         </input>
+                        TEST
                         <br/>
                         <input 
                             type="password" 
