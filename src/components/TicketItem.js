0a34b9a28ec45c58551b78f159507618b3739d79
MAThatcher <mitchellathatcher96@gmail.com>
2020-10-22 15:14:07 -0700

    work on ticket component

--- src/components/TicketItem.js
+++ src/components/TicketItem.js
@@ -1,11 +1,22 @@
 import React from 'react'
+
 function TicketItem (props) {
+
+    function openTicket (){
+        const {userId, userName,ticketId} = props
+
+        // TODO: send to .ticket
+        //this.props.history.push("/Ticket",{userId,userName,ticketId})               
+    }
+
+
+
     return(
             <tr>
                 <td>{props.date}</td>
                 <td>{props.message}</td>
                 <td>{props.ticketId}</td>             
-                <td><button className="btn  btn-primary ">Open Ticket</button></td>
+                <td><button className="btn  btn-primary " onClick={openTicket}>Open Ticket</button></td>
             </tr>
     )
 }
